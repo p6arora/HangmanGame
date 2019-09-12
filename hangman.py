9467ee10bc18ecdd45382771839df984d51e243b
Paarth Arora <paarth.arora21@gmail.com>
2019-09-12 01:40:47 -0400

    Implemented logic and display. Still need to clean up UI and debug execution errors

--- hangman.py
+++ hangman.py
@@ -7,6 +7,11 @@ Keep notifying the user of the remaining turns.
 Text File for input
 
 '''
+# TODO: make a graphic representation of handman
+
+# TODO: graphic blanks too close and not being overwritten
+
+import sys
 
 #read word from text-file
 def get_word():
@@ -27,15 +32,18 @@ def start_game(word):
     letters_remaining = len(word)
     letters_guessed = []
 
-    count = count_letters_in_word()
+    count = count_letters_in_word(word)
+    shown_word = " _ " * len(word)
 
-    display_word()
+    display_word(shown_word)
 
     # while loop till objects_drawn == 6 or letters_remaining == 0
     while (parts_drawn != 6 or letters_remaining == 0):
+    # TODO: have to break out of letters_remaining max
+    # TODO: not exiting game when you win
 
         # Ask user to enter letter
-        letter = input("Guess a letter..."
+        letter = input("Guess a letter...")
         # TODO: put both letter and word in lowercase
 
         while letter in letters_guessed:
@@ -46,8 +54,14 @@ def start_game(word):
         if letter in count:
             # See how many times letter has repeated
             # fill in letters at those index
-            word.find(letter)
+            occ = [pos for pos, char in enumerate(word) if char == letter]
+            shown_word_list = list(shown_word)
 
+            for i in occ:
+                shown_word_list[i] = letter
+
+            shown_word = ''.join(shown_word_list)
+            letters_remaining -= len(occ)
             letters_guessed.append(letter)
         # if wrong
         else:
@@ -60,14 +74,22 @@ def start_game(word):
 
 
         # display word
-        display_word()
+        display_word(shown_word)
 
-def display_word():
-    print("filler")
+    print("Game Over")
+    sys.exit()
+
+
+def display_word(shown_word):
+    print (shown_word)
 
 def count_letters_in_word(word):
-    print("filler")
     count = {}
+    for l in word:
+        if l in count:
+            count[l] += 1
+        else:
+            count[l] = 1
 
     return count
 
